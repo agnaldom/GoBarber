993861d9410978c2a1ee2fc03ab87a2e68e06070
Agnaldo marinho <agnaldomarinho7@gmail.com>
2020-02-18 19:26:57 -0300

    Update control user add returno infos

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -8,9 +8,15 @@ class UserController {
       return res.status(400).json({ error: 'User already exists:' });
     }
 
-    const user = await User.create(req.body);
+    // limitando o retorno do cadastro do usuario
+    const { id, name, email, provider } = await User.create(req.body);
 
-    return res.json(user);
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
