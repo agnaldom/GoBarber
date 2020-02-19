8e95f8b91c8ca5aecc24a03ebba3cb64c07b69cf
Agnaldo marinho <agnaldomarinho7@gmail.com>
2020-02-18 22:40:09 -0300

    finalizando Cadastro e autenticacao do usuarios

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -18,6 +18,34 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    // Teste se o email é  diferente do atual
+    if (email && email != user.email) {
+      const userExists = await User.findOne({ where: { email }});
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exist' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
