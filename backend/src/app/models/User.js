36d3f9940a0701f9caf556e068404ef2e9928e45
Agnaldo marinho <agnaldomarinho7@gmail.com>
2020-02-19 22:21:34 -0300

    Config avatar user

--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -25,6 +25,11 @@ class User extends Model {
     return this;
 
   }
+
+  static associat(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
